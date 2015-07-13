c62e2fc707dcd704e0a2eee5b1028d25275b1484
Sowjanya.Koripella <sowjanya.koripella@redvector.com>
2015-07-13 21:06:35 +0000

    Finish log in/log out

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -64,14 +64,15 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
-    test "email addresses should be saved as lower-case" do
+ test "email addresses should be saved as lower-case" do
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
-
   end
-
+ test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 
 end
